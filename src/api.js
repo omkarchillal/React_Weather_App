52cd225b63d2b128314b5ec65c62394fed9ee2b2
omkar chillal <omkarchillal99@gmail.com>
2023-03-11 10:21:43 +0530

    React_Weather_App

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '077edf9c82msh76158c42c2f40c2p1773d7jsndb1920ac17dd',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "0010d0ee1849bf8ef777bf3923520c11"
